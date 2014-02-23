934c9332d0706e3209e09d3dd05ae89ea1d16fcf
Nick Marsh <nickmarsh@gmail.com>
2014-02-22 18:16:39 -0800

    Added missing test files

--- scrobbler/Scrobbler.js
+++ scrobbler/Scrobbler.js
@@ -2,28 +2,66 @@
 
 
 //import _ = require("underscore");
+/*
+interface ScrobblerStationData {
+nowPlayingSong: song.Song;
+nowPlayingStarted: number; // timestamp
+};
+*/
 var Scrobbler = (function () {
+    // keep map of station -> { nowPlaying (Song), nowPlayingStart (moment), lastScrobble (Song), lastScrobbleStart (moment), consecutiveUpdateErrors (number), nowPlayingLastPosted (moment) }
+    // put this in an interface called ScrobblerStationData
+    //private stationData:{ [index: string]: ScrobblerStationData; }
     function Scrobbler(scrapers) {
         this.scrapers = scrapers;
     }
     Scrobbler.prototype.scrapeAndScrobble = function (stations) {
         for (var i = 0; i < stations.length; i++) {
-            //console.log(stations[i].StationName + " " + stations[i].ScraperName);
-            var scraper = this.scrapers[stations[i].ScraperName];
-            if (scraper) {
-                scraper.fetchAndParse(function (err, song) {
-                    if (!err) {
-                        if (song) {
-                            console.log("Scraped song " + song.Artist + " - " + song.Track);
-                        } else {
-                            console.log("Could not scrape song");
-                        }
+            var scraperName = stations[i].ScraperName;
+            var scraper = this.scrapers[scraperName];
+
+            scraper.fetchAndParse(function (err, song) {
+                if (!err) {
+                    if (song) {
+                        console.log("Scraped song " + song.Artist + " - " + song.Track);
+                    } else {
+                        console.log("Could not scrape song");
                     }
-                    // Assume error logging is done by scraper
-                });
-            } else {
-                //todo log
+                }
+            });
+            /*
+            if (!scraper) {
+            // log
+            continue;
             }
+            
+            var stationData = this.stationData[scraperName];
+            if (!stationData) {
+            stationData = {..};
+            this.stationData[scraperName] = stationData;
+            }
+            
+            (scraperName) => {
+            scraper.fetchAndParse((err, song) => {
+            // [Make sure errors only happen in rare & unexpected circumstances (e.g. 404 rather than song not found)
+            
+            if (err) {
+            continue
+            winston.error("Error scraping " + scraperName)
+            }
+            // if station was last updated a long time ago, clear the data for the station
+            // update station last processed time to now
+            
+            // if song is different to previous song
+            // scrobble previous song if it's valid and its start time was long enough ago
+            // update now playing song to current song and now playing start to now
+            // if the new now playing song is valid, post now playing
+            
+            // [just need to store now playing song and when it started for each station]
+            
+            });
+            }(scraperName);
+            */
         }
     };
     return Scrobbler;
--- scrobbler/Scrobbler.ts
+++ scrobbler/Scrobbler.ts
@@ -2,32 +2,84 @@ import stat = require("./Station");
 import scrap = require("./scrapers/Scraper");
 
 //import _ = require("underscore");
+/*
+interface ScrobblerStationData {
+	nowPlayingSong: song.Song;
+	nowPlayingStarted: number; // timestamp
+};
+*/
 
 export class Scrobbler {
+
+	// keep map of station -> { nowPlaying (Song), nowPlayingStart (moment), lastScrobble (Song), lastScrobbleStart (moment), consecutiveUpdateErrors (number), nowPlayingLastPosted (moment) }
+	// put this in an interface called ScrobblerStationData
+
+	//private stationData:{ [index: string]: ScrobblerStationData; }
 
 	constructor(private scrapers:{ [index: string]: scrap.Scraper; } ) {}
 
 	scrapeAndScrobble(stations:stat.Station[]): void {
-		//console.log("testing " + this.test + " " + x);
 		for (var i = 0; i < stations.length; i++) {
-			//console.log(stations[i].StationName + " " + stations[i].ScraperName);
-			var scraper = this.scrapers[stations[i].ScraperName];
-			if (scraper) {
+
+			var scraperName = stations[i].ScraperName;
+			var scraper = this.scrapers[scraperName];
+
+
+			scraper.fetchAndParse((err, song) => {
+			    if (!err) {
+			        if (song) {
+				        console.log("Scraped song " + song.Artist + " - " + song.Track);
+				    }
+				    else {
+				        console.log("Could not scrape song");
+				    }
+			    }
+			});
+
+			/*
+			if (!scraper) {
+				// log
+				continue;
+			}
+
+			var stationData = this.stationData[scraperName];
+			if (!stationData) { 
+				stationData = {..};
+				this.stationData[scraperName] = stationData;
+			}
+
+			(scraperName) => {
 				scraper.fetchAndParse((err, song) => {
-					if (!err) {
-						if (song) {
-							console.log("Scraped song " + song.Artist + " - " + song.Track);
-						}
-						else {
-							console.log("Could not scrape song");
-						}
-					}
-					// Assume error logging is done by scraper
+					// [Make sure errors only happen in rare & unexpected circumstances (e.g. 404 rather than song not found)
+
+					if (err) {
+						continue
+						winston.error("Error scraping " + scraperName)
+					} 
+					// if station was last updated a long time ago, clear the data for the station
+					// update station last processed time to now
+
+					// if song is different to previous song
+						// scrobble previous song if it's valid and its start time was long enough ago
+						// update now playing song to current song and now playing start to now
+					// if the new now playing song is valid, post now playing
+
+					// [just need to store now playing song and when it started for each station]
+
 				});
-			}
-			else {
-				//todo log
-			}
+			}(scraperName);
+			*/
 		}
 	}
 }
+
+/* 
+possible sequences
+null -> song1 -> null (should have posted now playing once but not scrobbled, song1 was not playing long enough)
+null -> song1 -> song1 -> null (should have posted now playing twice and scrobbled song1)
+song1 -> song1 -> song2 (should have posted now playing 3 times and scrobbled song1)
+song1 -> song2 -> song3 (should have posted now playing 3 times and not scrobbled)
+song1 -> error -> song1 -> null (should post now playing twice and scrobbled song1)
+song1 -> error -> song2 -> null (should post now playing twice and scrobbled song1)
+song1 -> error -> error -> error -> etc -> song1 -> null (should post now playing twice and not scrobbled)
+*/
